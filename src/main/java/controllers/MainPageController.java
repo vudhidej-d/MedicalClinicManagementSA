32cd43727f5cf251166f6f4e11f7d532d655d9b2
VUDHIDEJ.D <vudhidej.d@ku.th>
2017-12-14 00:55:38 +0700

    init

---
+++ src/main/java/controllers/MainPageController.java
@@ -0,0 +1,42 @@
+package controllers;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class MainPageController {
+
+    @FXML
+    private Pane mainPane;
+
+    @FXML
+    public void medicalRecordsBtnHandle() {
+        changeScene("/MedicalRecordsPage.fxml", 1000, 800);
+    }
+
+    @FXML
+    public void examinationRoomBtnHandle() {
+        changeScene("/ExaminationRoomPage.fxml", 1000, 800);
+    }
+
+    @FXML
+    public void dispensaryBtnHandle() {
+        changeScene("/DispensaryPage.fxml", 1000, 800);
+    }
+
+    public void changeScene(String scene, int w, int h) {
+        Stage stage = (Stage) mainPane.getScene().getWindow();
+        FXMLLoader loader = new FXMLLoader(getClass().getResource(scene));
+        try {
+            stage.setScene(new Scene((Parent) loader.load(),w, h));
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
